127b57fbfeb89b0d7e494e349c100702383d6a80
olgaluisa <luiisaolga11@gmail.com>
2025-06-13 23:59:21 -0300

    feat: create initial component structure with states and list typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+import { useState } from 'react';
+
+export type ListType = {
+  id: number;
+  title: string;
+  status: boolean;
+};
+
 function App() {
+  const [list, setList] = useState<ListType[]>([]);
+  const [input, setInput] = useState('');
+  const [updateInput, setUpdateInput] = useState('');
+  const [selectId, setSelectId] = useState<number | null>(null);
+  const [modal, setModal] = useState(false);
+
   return <></>;
 }
 
